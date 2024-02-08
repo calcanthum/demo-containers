b422543383de50d420abfe7de4e40dbc16c1f4d2
Ivan Sabljak <108417742+calcanthum@users.noreply.github.com>
2024-02-08 15:26:09 +0200

    Create cookie_settings_test_http.php

---
+++ snuff/checklist/cookie_settings_test_http.php
@@ -0,0 +1,16 @@
+<?php
+setcookie("testCookie", "testValue", [
+    'expires' => time() + 3600,
+    'path' => '/',
+    'secure' => true, 
+    'httponly' => true,
+    'samesite' => 'Lax' // Explicitly set for demonstration, Snuffleupagus should enforce this
+]);
+
+if (isset($_COOKIE['testCookie'])) {
+    echo "Cookie 'testCookie' is set.<br/>";
+    echo "Manual verification needed: Check browser's DevTools to confirm 'SameSite=Lax' attribute.";
+} else {
+    echo "Cookie 'testCookie' is not set. Something went wrong.";
+}
+?>
